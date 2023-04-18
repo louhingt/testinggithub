af76dcfb70ca56b32af6c4af93c732d28cdb5774
louhingt <131090954+louhingt@users.noreply.github.com>
2023-04-18 08:29:47 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.forever(function () {
+	
+})
